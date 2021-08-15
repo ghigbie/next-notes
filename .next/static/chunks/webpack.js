ac8f07f241d1d978ab1518105b3f2b2e61a45926
ghigbie <gehigbie@gmail.com>
2021-08-15 14:12:41 -0400

    Add readme

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e673e92e99c61c41e3b"; }
+/******/ 		__webpack_require__.h = function() { return "5524076a8c65ae8b97d1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ README.md
@@ -0,0 +1,3 @@
+# NextJS Notes
+
+A simple notes app built in NextJS (with ReactJS and Javascript)
